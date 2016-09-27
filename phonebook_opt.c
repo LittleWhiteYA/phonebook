a058a74883fc2984f955957bc79a383fefc56d86
LittleWhiteYA <F74014041@mail.ncku.edu.tw>
2016-09-27 17:37:28 +0800

    add hash function

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -1,17 +1,21 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 #include "phonebook_opt.h"
 
 /* FILL YOUR OWN IMPLEMENTATION HERE! */
 entry *findName(char lastName[], entry *pHead)
 {
+    /*
     while (pHead != NULL) {
         if (strcasecmp(lastName, pHead->lastName) == 0)
             return pHead;
         pHead = pHead->pNext;
     }
     return NULL;
+    */
+    return hash_table[BKDRHash(lastName)];
 }
 
 entry *append(char lastName[], entry *e)
@@ -22,6 +26,19 @@ entry *append(char lastName[], entry *e)
     strcpy(e->lastName, lastName);
     e->pNext = NULL;
 
+    unsigned int idx = BKDRHash(lastName);
+    hash_table[idx] = e;
+
     return e;
 }
 
+unsigned int BKDRHash(char *str)
+{
+    unsigned int seed = 131;
+    unsigned int hash = 0;
+    while(*str) {
+        hash = hash * seed + (*str++);
+    }
+
+    return (hash & 0x7FFFFFFF);
+}
--- phonebook_opt.h
+++ phonebook_opt.h
@@ -2,11 +2,12 @@
 #define _PHONEBOOK_H
 
 #define MAX_LAST_NAME_SIZE 16
+#define MAX_UNSIGNED_INT 3000000000
 
 #define OPT 1
 typedef struct __PHONE_BOOK_ENTRY {
     char lastName[MAX_LAST_NAME_SIZE];
-	struct __NAME_DETAIL *detail;
+    struct __NAME_DETAIL *detail;
     struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
 
@@ -22,7 +23,10 @@ typedef struct __NAME_DETAIL {
     char zip[5];
 } detail;
 
+entry *hash_table[MAX_UNSIGNED_INT];
+
 entry *findName(char lastName[], entry *pHead);
 entry *append(char lastName[], entry *e);
+unsigned int BKDRHash(char *str);
 
 #endif
